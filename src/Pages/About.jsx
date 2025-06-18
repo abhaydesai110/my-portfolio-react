e6348b8d99a1c76d6bec37c6ea2a6635c00562bb
Brijesh Patadiya AD <brijesh.patadiya@weenggs.in>
2025-06-18 22:26:18 +0530

    resume updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Resume from "../assets/img/AbhayDesai_3_Year_Exp_React_Js.pdf";
 
 const About = () => {
@@ -7,27 +7,38 @@ const About = () => {
       <div className="bg-grey-50" id="about">
         <div className="container flex flex-col items-center py-16 md:py-20 lg:flex-row">
           <div className="w-full text-center sm:w-3/4 lg:w-3/5 lg:text-left">
-            <h2 className="font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-              Who am I?
-            </h2>
-            <h4 className="pt-6 font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
-              I am Abhay Desai, a React Js Developer
-            </h4>
+            <h2 className="font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">Who am I?</h2>
+            <h4 className="pt-6 font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">I am Abhay Desai, a React Js Developer</h4>
             <p className="pt-6 font-body leading-relaxed text-grey-20">
-              React JS Developer with 2+ years of experience in building user
-              interfaces and implementing components. Proven ability to work
-              independently and as part of a team to deliver high-quality
-              products. Strong technical skills in JavaScript, HTML, CSS, and
-              React JS frameworks.
+              React JS Developer with 3.5+ years of experience in building user interfaces and implementing components. Proven ability to work independently and as part of a team to deliver high-quality
+              products. Strong technical skills in JavaScript, HTML, CSS, and React JS frameworks.
             </p>
-            <h3 className="pt-6 font-header text-xl font-medium text-black sm:text-2xl lg:text-2xl">
-              Technical skills
-            </h3>
-            <p className="flex pt-6 font font-medium text-grey-20 flex-row">
-              HTML | CSS | JAVASCRIPT | REACT | STATE MANAGEMENT | ROUTING
-              <br />| API INTEGRATON | DEBUGGING| DEPLOYMENT | NODEJS | EXPRESS
-              JS | <br />| MONGODB | REDUX-TOOLKIT
-            </p>
+            <h3 className="pt-6 font-header text-xl font-medium text-black sm:text-2xl lg:text-2xl">Technical skills</h3>
+            <div className="pt-6 text-grey-10 font-medium space-y-4">
+              <div>
+                <h3 className="text-lg font-semibold mb-1 text-black">Frontend</h3>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">HTML5, CSS3, SCSS, JavaScript, TypeScript</p>
+                <p className="flex flex-wrap gap-x-4 gap-y-2"> React.js, Next.js, Bootstrap, Tailwind CSS, Material UI, Chakra UI, Shadcn, Ant Design</p>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">State Management & Routing: Redux, Redux Toolkit, Context API, React Router</p>
+              </div>
+
+              <div>
+                <h3 className="text-lg font-semibold mb-1 text-black">Backend</h3>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">Node.js, Express.js, Socket.io</p>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">API Development: REST APIs, 3rd Party Integrations (Stripe, Plaid, Google Maps API, Google Drive, Dropbox)</p>
+              </div>
+
+              <div>
+                <h3 className="text-lg font-semibold mb-1 text-black">Tools & Workflow</h3>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">Git, GitHub, Postman, JIRA, Trello, Confluence</p>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">Figma, Shadcn UI, SCSS</p>
+              </div>
+
+              <div>
+                <h3 className="text-lg font-semibold mb-1 text-black">Additional Skills</h3>
+                <p className="flex flex-wrap gap-x-4 gap-y-2">API Integration, Debugging, Deployment, Google Drive & Dropbox Upload, Payment Integration (Stripe, Plaid)</p>
+              </div>
+            </div>
             <h3 className="pt-6 font-header flex items-center justify-start text-xl font-medium sm:justify-center justify-start text-black sm:text-2xl lg:text-2xl">
               <i className="bx bxl-github text-4xl text-black duration-300 hover:text-grey-20"></i>
               <p className="text-center">GitHub Desktop Proficiency</p>
@@ -35,11 +46,8 @@ const About = () => {
             <p className="pt-6 font-body leading-relaxed text-grey-20">
               GITHUB PROFICIENCY <br />
               • Proficient in Git and GitHub, including branch creation, merging, and conflict resolution <br />
-              • Experienced with continuous integration and delivery environments, including development,
-              staging, and UAT<br />
-              •
-              Creation and merging of branches for feature development and bug fixes
-              Merging of branches with conflict resolution to maintain code integrity and project continuity
+              • Experienced with continuous integration and delivery environments, including development, staging, and UAT
+              <br />• Creation and merging of branches for feature development and bug fixes Merging of branches with conflict resolution to maintain code integrity and project continuity
             </p>
             <div className="flex items-center space-x-7 justify-center w-full pt-6">
               <button className="text-xs md:text-lg xs:w-40 flex font-normal items-center space-x-2 justify-center hover:w-44  h-12 w-40   hover:bg-primary hover:border-0 duration-300  font-medium bg-black text-white rounded-3xl  ">
@@ -48,11 +56,7 @@ const About = () => {
                   <h2>View Github</h2>
                 </Link>
               </button>
-              <Link
-                to={Resume}
-                download="Abhay_ReactJs_2YearsExpAbhayDesai_3_Year_Exp_React_Js"
-                target="_black"
-              >
+              <Link to={Resume} download="Abhay_ReactJs_2YearsExpAbhayDesai_3_Year_Exp_React_Js" target="_black">
                 <button className="btn flex items-center justify-center  classes.navLinks text-xs md:text-lg md:w-40 w-36 h-12 ">
                   <span className="">Download CV</span>
                 </button>
@@ -60,53 +64,28 @@ const About = () => {
             </div>
             <div className="flex mt-6 flex-col justify-center pt-6 sm:flex-row lg:justify-start">
               <div className="flex items-center justify-center sm:justify-start">
-                <p className="font-body text-lg font-semibold uppercase text-grey-20">
-                  Connect with me
-                </p>
+                <p className="font-body text-lg font-semibold uppercase text-grey-20">Connect with me</p>
                 <div className="hidden sm:block">
                   <i className="bx bx-chevron-right text-2xl text-primary"></i>
                 </div>
               </div>
               <div className="flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0">
-                <Link
-                  to="https://www.facebook.com/abhay.desai.9028"
-                  target="_black"
-                >
+                <Link to="https://www.facebook.com/abhay.desai.9028" target="_black">
                   <i className="bx bxl-facebook-square text-2xl text-primary hover:text-black duration-300"></i>
                 </Link>
-                <Link
-                  to="https://www.twitter.com/abhaydesai110"
-                  target="_black"
-                  className="pl-4"
-                >
+                <Link to="https://www.twitter.com/abhaydesai110" target="_black" className="pl-4">
                   <i className="bx bxl-twitter text-2xl text-primary hover:text-black duration-300"></i>
                 </Link>
-                <Link
-                  to="https://abhaydesai.netlify.app/"
-                  className="pl-4"
-                  target="_black"
-                >
+                <Link to="https://abhaydesai.netlify.app/" className="pl-4" target="_black">
                   <i className="bx bxl-dribbble text-2xl text-primary hover:text-black  duration-300"></i>
                 </Link>
-                <Link
-                  to="https://www.linkedin.com/in/abhay-desai-336863166/"
-                  className="pl-4"
-                  target="_black"
-                >
+                <Link to="https://www.linkedin.com/in/abhay-desai-336863166/" className="pl-4" target="_black">
                   <i className="bx bxl-linkedin text-2xl text-primary hover:text-black  duration-300"></i>
                 </Link>
-                <Link
-                  to="https://www.instagram.com/abhay_desai_110/"
-                  className="pl-4"
-                  target="_blank"
-                >
+                <Link to="https://www.instagram.com/abhay_desai_110/" className="pl-4" target="_blank">
                   <i className="bx bxl-instagram text-2xl text-primary hover:text-black duration-300"></i>
                 </Link>
-                <Link
-                  to="https://github.com/abhaydesai110"
-                  className="pl-4 "
-                  target="_black"
-                >
+                <Link to="https://github.com/abhaydesai110" className="pl-4 " target="_black">
                   <i className="bx bxl-github text-2xl text-primary hover:text-black  duration-300"></i>
                 </Link>
               </div>
@@ -116,12 +95,8 @@ const About = () => {
             <div>
               <div className="md:pt-18 pt-6 ">
                 <div className="flex items-end justify-between">
-                  <h4 className="font-body font-semibold uppercase text-black">
-                    Github Desktop
-                  </h4>
-                  <h3 className="font-body text-3xl font-bold text-primary">
-                    95%
-                  </h3>
+                  <h4 className="font-body font-semibold uppercase text-black">Github Desktop</h4>
+                  <h3 className="font-body text-3xl font-bold text-primary">95%</h3>
                 </div>
                 <div className="mt-2 h-3 w-full rounded-full bg-lila">
                   <div className="h-3 rounded-full bg-primary w-[95%]"></div>
@@ -129,12 +104,8 @@ const About = () => {
               </div>
               <div className="md:pt-18 pt-6 ">
                 <div className="flex items-end justify-between">
-                  <h4 className="font-body font-semibold uppercase text-black">
-                    HTML
-                  </h4>
-                  <h3 className="font-body text-3xl font-bold text-primary">
-                    85%
-                  </h3>
+                  <h4 className="font-body font-semibold uppercase text-black">HTML</h4>
+                  <h3 className="font-body text-3xl font-bold text-primary">85%</h3>
                 </div>
               </div>
               <div className="mt-2 h-3 w-full rounded-full bg-lila">
@@ -143,12 +114,8 @@ const About = () => {
             </div>
             <div className="md:pt-18 pt-6 ">
               <div className="flex items-end justify-between">
-                <h4 className="font-body font-semibold uppercase text-black">
-                  CSS
-                </h4>
-                <h3 className="font-body text-3xl font-bold text-primary">
-                  70%
-                </h3>
+                <h4 className="font-body font-semibold uppercase text-black">CSS</h4>
+                <h3 className="font-body text-3xl font-bold text-primary">70%</h3>
               </div>
               <div className="mt-2 h-3 w-full rounded-full bg-lila">
                 <div className="h-3 rounded-full bg-primary w-[70%]"></div>
@@ -156,12 +123,8 @@ const About = () => {
             </div>
             <div className="md:pt-18 pt-6 ">
               <div className="flex items-end justify-between">
-                <h4 className="font-body font-semibold uppercase text-black">
-                  Javascript
-                </h4>
-                <h3 className="font-body text-3xl font-bold text-primary">
-                  80%
-                </h3>
+                <h4 className="font-body font-semibold uppercase text-black">Javascript</h4>
+                <h3 className="font-body text-3xl font-bold text-primary">80%</h3>
               </div>
               <div className="mt-2 h-3 w-full rounded-full bg-lila">
                 <div className="h-3 rounded-full bg-primary w-[80%]"></div>
@@ -169,12 +132,8 @@ const About = () => {
             </div>
             <div className="md:pt-18 pt-6 ">
               <div className="flex items-end justify-between">
-                <h4 className="font-body font-semibold uppercase text-black">
-                  React
-                </h4>
-                <h3 className="font-body text-3xl font-bold text-primary">
-                  91%
-                </h3>
+                <h4 className="font-body font-semibold uppercase text-black">React</h4>
+                <h3 className="font-body text-3xl font-bold text-primary">91%</h3>
               </div>
               <div className="mt-2 h-3 w-full rounded-full bg-lila">
                 <div className="h-3 rounded-full bg-primary w-[91%]"></div>
--- src/Pages/MyExperience.jsx
+++ src/Pages/MyExperience.jsx
@@ -1,22 +1,17 @@
-import React from "react";
 import ScalelotLogo from "../../src/assets/img/ScalelotLogo2.jpg";
 import JeevesLogo from "../../src/assets/img/JeevesLogo.svg";
 // import Webytude from "../assets/img/webytude-logo.png";
 import Webytude from "../assets/img/capsync-logo.svg";
-import { Link } from "react-router-dom";
 
 const MyExperience = () => {
   return (
     <div className=" bg-[url('../src/assets/img/Good.jpg')] bg-fixed bg-cover bg-cover bg-fixed">
       <div className="container   py-16 md:py-20" id="work">
-        <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-          My work experience
-        </h2>
+        <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">My work experience</h2>
         <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl"></h3>
 
         <div className="relative mx-auto mt-12 flex w-full flex-col lg:w-2/3">
           <span className="left-2/5 absolute inset-y-0 ml-10 hidden w-0.5 bg-grey-40 md:block"></span>
-
 
           <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
             <div className="md:w-2/5">
@@ -36,12 +31,8 @@ const MyExperience = () => {
                 <div className="mt-1 flex">
                   <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
                   <div className="md:-mt-1 md:pl-8">
-                    <span className="block font-body font-bold text-black">
-                      February 2024 - Present
-                    </span>
-                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                      Software Developer (Capsync)
-                    </span>
+                    <span className="block font-body font-bold text-black">February 2024 - Present</span>
+                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">Software Developer (Capsync)</span>
                     <div className="pt-2">
                       <span className="block font-body text-black">
                         I am currently working as a software developer at Capsync, specializing in React.js, Node.js, and PostgreSQL, developing a fintech product for the company.
@@ -70,18 +61,12 @@ const MyExperience = () => {
                 <div className="mt-1 flex">
                   <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
                   <div className="md:-mt-1 md:pl-8">
-                    <span className="block font-body font-bold text-black">
-                      Auguset 2022 - February 2024
-                    </span>
-                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                      React Js Developer (SCALELOT TECHNOLOGY)
-                    </span>
+                    <span className="block font-body font-bold text-black">January 2022 - February 2024</span>
+                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">React Js Developer (SCALELOT TECHNOLOGY)</span>
                     <div className="pt-2">
                       <span className="block font-body text-black">
-                        As a React.js developer at Scalelot Technology, I create
-                        and maintain scalable web applications, utilizing
-                        cutting-edge technologies to deliver exceptional user
-                        experiences and robust functionality.
+                        As a React.js developer at Scalelot Technology, I create and maintain scalable web applications, utilizing cutting-edge technologies to deliver exceptional user experiences and
+                        robust functionality.
                       </span>
                     </div>
                   </div>
@@ -107,18 +92,11 @@ const MyExperience = () => {
                 <div className="mt-1 flex">
                   <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
                   <div className="md:-mt-1 md:pl-8">
-                    <span className="block font-body font-bold text-black">
-                      August 2018 - December 2021
-                    </span>
-                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                      Team Leader (JEEVES CONSUMER SERVICES)
-                    </span>
+                    <span className="block font-body font-bold text-black">August 2018 - December 2021</span>
+                    <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">Tech Support (JEEVES CONSUMER SERVICES)</span>
                     <div className="pt-2">
                       <span className="block font-body text-black">
-                        As Team Leader at Jeeves Consumer Services, I excelled
-                        in managing electronics repair teams, optimizing
-                        workflows, and achieving exceptional customer
-                        satisfaction through efficient service delivery.
+                        I worked as a Technical Support Assistant at Jeeves Consumer Support, helping field technicians with troubleshooting and repair guidance for home appliances.
                       </span>
                     </div>
                   </div>
